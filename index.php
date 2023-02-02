9e94a14ffb1a3abf5cec8343395c9ddd26c19e03
JKakuk <josh_kakuk1@hotmail.ca>
2023-02-01 20:01:07 -0700

    start of log in page and registration page

--- index.php
+++ index.php
@@ -1,6 +1,56 @@
+<html>
+    <head>
+        <title>
+            Login
+        </title>
+
+        <style>
+            h1{
+                text-align: center;
+
+            }
+
+            body{
+                text-align: center;
+            }
+
+        </style>
+    </head>
+
+
+
+
+
+<H1>
+  Login
+</H1>
+<body>
+
+
+
+<form>
+
+   User Name: <input type='txt' name='username'><br>
+    Password: <input type='txt' name='password'><br>
+              <input type='submit' name='login' value='login'>
+            
+
+
+</form>
+
+
+
+
 <?php
+    if(isset($_POST['login'])){
+        
+    }
 
 
 
 
 ?>
+
+
+</body>
+</html>
