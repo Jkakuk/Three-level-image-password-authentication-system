20af22baf6ba29d11cdd11cb7670353fbde9de81
Jkakuk <josh_kakuk1@hotmail.ca>
2023-02-02 23:47:03 -0700

    Commit from laptop

---
+++ Registration.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <title>
+            Registration
+        </title>
+
+        <style>
+            h1{
+                text-align: center;
+
+            }
+
+            body{
+                text-align: center;
+            }
+
+        </style>
+    </head>
+
+
+
+
+
+<H1>
+  Registration
+</H1>
+<body>
+
+
+
+<form>
+
+   User Name: <input type='txt' name='username'><br>
+    Password: <input type='txt' name='password'><br>
+    Re-Type Password: <input type='txt' name='password'><br>
+              <input type='submit' name='submit' value='submit'>
+            
+
+
+</form>
+
+
+
+
+<?php
+    if(isset($_POST['submit'])){
+        
+    }
+
+
+
+
+?>
+
+
+</body>
+</html>
